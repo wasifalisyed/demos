70a2d50fe3ab3038f1c52c032edb08bf18b97c37
Wasif.Ali <Wasif.Ali@WASIF-ALI>
2017-07-10 18:58:26 +0500

    first commit

---
+++ DashboardApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using DashboardApp.Models;
+
+namespace DashboardApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      var db = new DashboardAppEntities();
+      var dashboardReport = new DashboardReport
+      {
+        NewComments = db.Comments.Count(),
+        NewTasks = db.Tasks.Count(),
+        NewOrders = db.Orders.Count(),
+        SupportTickets = db.SupportTickets.Count()
+      };
+
+      ViewBag.Title = "Home";
+      return View(dashboardReport);
+    }
+  }
+}
