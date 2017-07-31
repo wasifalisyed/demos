dc00f6264c0481a52e40978a396f4d9023ecd4cd
Wasif.Ali <Wasif.Ali@WASIF-ALI>
2017-07-31 12:32:32 +0500

    custom errors

--- DashboardApp/Controllers/HomeController.cs
+++ DashboardApp/Controllers/HomeController.cs
@@ -2,12 +2,14 @@
 using System.Web.Mvc;
 using DashboardApp.Models;
 
+
 namespace DashboardApp.Controllers
 {
   public class HomeController : Controller
   {
     public ActionResult Index()
     {
+            
             var db = new DashboardAppEntities();
             var dashboardReport = new DashboardReport
             {
